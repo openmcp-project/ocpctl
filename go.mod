a8f27f49a402fca0fd6b153c1c947794841e44a9
Nico Andres <nico.andres@sap.com>
2026-07-28 12:08:13 +0200

    env test
    On-behalf-of: @SAP nico.andres@sap.com
    Signed-off-by: Nico Andres <nico.andres@sap.com>

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/openmcp-project/cluster-provider-kind v0.6.0
 	github.com/openmcp-project/openmcp-operator/api v1.3.0
 	github.com/spf13/cobra v1.10.2
-	github.com/stretchr/testify v1.11.1
 	go.uber.org/zap v1.28.0
 	k8s.io/api v0.36.3
 	k8s.io/apimachinery v0.36.3
@@ -78,7 +77,6 @@ require (
 	google.golang.org/protobuf v1.36.12-0.20260120151049-f2248ac996af // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.13.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.36.2 // indirect
 	k8s.io/klog/v2 v2.140.0 // indirect
 	k8s.io/kube-openapi v0.0.0-20260603220949-865597e52e25 // indirect
--- pkg/config/environment_test.go
+++ pkg/config/environment_test.go
@@ -40,3 +40,113 @@ func TestDefaultContainsExpectedComponents(t *testing.T) {
 		}
 	}
 }
+
+func TestLoad(t *testing.T) {
+
+}
+
+func TestMergeComponents(t *testing.T) {
+	tests := []struct {
+		name    string
+		base    []ComponentSpec
+		overlay []ComponentSpec
+		want    []ComponentSpec
+	}{
+		{
+			name: "empty overlay returns base",
+			base: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"},
+			},
+			overlay: []ComponentSpec{},
+			want: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"},
+			},
+		},
+		{
+			name: "overlay overrides base for same component",
+			base: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"},
+			},
+			overlay: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.1.0"},
+			},
+			want: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.1.0"},
+			},
+		},
+		{
+			name: "empty image in overlay does not override base",
+			base: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"},
+			},
+			overlay: []ComponentSpec{
+				{Name: "sp-flux", Image: ""},
+			},
+			want: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"},
+			},
+		},
+		{
+			name: "additional component in overlay added to based",
+			base: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"},
+			},
+			overlay: []ComponentSpec{
+				{Name: "sp-crossplane", Image: "ghcr.io/openmcp-project/images/service-provider-crossplane:v1.0.0"},
+			},
+			want: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"},
+				{Name: "sp-crossplane", Image: "ghcr.io/openmcp-project/images/service-provider-crossplane:v1.0.0"},
+			},
+		},
+		{
+			name: "multiple base components overlay overrides one and adds one",
+			base: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"},
+				{Name: "sp-crossplane", Image: "ghcr.io/openmcp-project/images/service-provider-crossplane:v1.0.0"},
+				{Name: "sp-ocm", Image: "ghcr.io/openmcp-project/images/service-provider-ocm:v1.0.0"},
+			},
+			overlay: []ComponentSpec{
+				{Name: "sp-crossplane", Image: "ghcr.io/openmcp-project/images/service-provider-crossplane:v2.0.0"},
+				{Name: "sp-kro", Image: "ghcr.io/openmcp-project/images/service-provider-kro:v1.0.0"},
+			},
+			want: []ComponentSpec{
+				{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"},
+				{Name: "sp-crossplane", Image: "ghcr.io/openmcp-project/images/service-provider-crossplane:v2.0.0"},
+				{Name: "sp-ocm", Image: "ghcr.io/openmcp-project/images/service-provider-ocm:v1.0.0"},
+				{Name: "sp-kro", Image: "ghcr.io/openmcp-project/images/service-provider-kro:v1.0.0"},
+			},
+		},
+		{
+			name:    "nil overlay returns base",
+			base:    []ComponentSpec{{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"}},
+			overlay: nil,
+			want:    []ComponentSpec{{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"}},
+		},
+		{
+			name:    "nil base with overlay returns overlay",
+			base:    nil,
+			overlay: []ComponentSpec{{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"}},
+			want:    []ComponentSpec{{Name: "sp-flux", Image: "ghcr.io/openmcp-project/images/service-provider-flux:v1.0.0"}},
+		},
+		{
+			name:    "both nil returns nil",
+			base:    nil,
+			overlay: nil,
+			want:    nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := mergeComponents(tt.base, tt.overlay)
+			if len(got) != len(tt.want) {
+				t.Fatalf("len = %d, want %d", len(got), len(tt.want))
+			}
+			for i := range got {
+				if got[i] != tt.want[i] {
+					t.Errorf("[%d] = %+v, want %+v", i, got[i], tt.want[i])
+				}
+			}
+		})
+	}
+}
